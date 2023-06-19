d1240a33cc4f952e56d28d4af48f5c5ad5f1786a
Daichi1983 <77876382+Daichi1983@users.noreply.github.com>
2023-06-19 21:43:27 +0900

    2030619uploaded5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+import react from '@astrojs/react';
 import partytown from "@astrojs/partytown";
 
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    partytown({
-      // Adds dataLayer.push as a forwarding-event.
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
-});
+	integrations: [
+		// Enable Preact to support Preact JSX components.
+		preact(),
+		// Enable React for the Algolia search component.
+		react(),
+		partytown({
+			config: {
+			  forward: ["dataLayer.push"],
+			},
+		  }),
+	});
