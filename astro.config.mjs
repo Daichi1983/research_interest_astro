855e273d1c9c784b8590ec9cf34cc5a9c15abd0a
Daichi1983 <77876382+Daichi1983@users.noreply.github.com>
2023-06-19 21:24:49 +0900

    20230619uploaded

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
+import partytown from "@astrojs/partytown";
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,6 +11,10 @@ export default defineConfig({
 		preact(),
 		// Enable React for the Algolia search component.
 		react(),
+		partytown({
+			// Adds dataLayer.push as a forwarding-event.
+			config: {
+			  forward: ["dataLayer.push"],
 	],
 	site: `https://astro.build`,
 });
