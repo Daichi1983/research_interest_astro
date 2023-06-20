d9ed30b54131bea257ddc2352fb572a652efccfe
Daichi1983 <77876382+Daichi1983@users.noreply.github.com>
2023-06-20 21:55:15 +0900

    20230620GoogleSearchConsole

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,20 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
 import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		// Enable Preact to support Preact JSX components.
-		preact(),
-		// Enable React for the Algolia search component.
-		react(),
-		partytown({
-		  // Adds dataLayer.push as a forwarding-event.
-		  config: {
-			forward: ["dataLayer.push"],
-		  },
-		}),
-	],
-	site: `https://okinawa.pages.dev/`,
+  integrations: [
+  // Enable Preact to support Preact JSX components.
+  preact(),
+  // Enable React for the Algolia search component.
+  react(), partytown({
+    // Adds dataLayer.push as a forwarding-event.
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }),
+  sitemap()],
+  site: `https://okinawa.pages.dev/`
 });
